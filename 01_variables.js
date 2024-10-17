7870c889fb9cfbffc18263601c7ab838d4cc39a7
Prems27 <sharma.prem@yash.com>
2024-10-17 22:29:37 +0530

    added conversion and datatypes file

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 13425
+let accountEmail = "prem@gmail.com"
+var accountPassword = "6789"
+accountCity = "Amravati"
+let accountState;
+
+// accountId = 342
+accountEmail = "abc@yahoo.com"
+accountPassword = "132456"
+accountCity = "Pune"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//alert(3 + 3) // we are using nodejs and not browser
+
+console.log("Prem");
+
+console.log(3 + 3);
+
+let name = "Prem"
+let age = 18
+let isLoggedIn =  false
+let state = null
+
+//DATA TAYPES in JS
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+
