7fec3d85ad86631b3ef12ecc6f2c4c05e45368b2
Prems27 <sharma.prem@yash.com>
2024-10-18 15:46:20 +0530

    DataType Summary Added

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -16,3 +16,69 @@ let someNumber = 33;
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+// ******************** Operations *************// 
+
+
+let value = 3
+
+let negValue = -value;
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Prem"
+
+let str2 = " Sharma"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log(1+ "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 +  2 + "2");
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
---
+++ dataTypes_summary.js
@@ -0,0 +1,28 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('132')
+const anotherId = Symbol('132')
+
+console.log(id === anotherId);
+
+const bigNumber = 324872387437n;
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Prem",
+    age: 28,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+
+
